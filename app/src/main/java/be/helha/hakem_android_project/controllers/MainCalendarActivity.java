7e18198715e3a63b96d72086dd5bb6bae1fbb6ab
TheoHakk <102266628+TheoHakk@users.noreply.github.com>
2023-10-23 23:57:16 +0200

    design terminé

--- app/src/main/java/be/helha/hakem_android_project/controllers/MainCalendarActivity.java
+++ app/src/main/java/be/helha/hakem_android_project/controllers/MainCalendarActivity.java
@@ -1,15 +1,37 @@
 package be.helha.hakem_android_project.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import be.helha.hakem_android_project.R;
 
 public class MainCalendarActivity extends AppCompatActivity {
+
+    FloatingActionButton addTreatment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calendar_screen);
+        setContentView(R.layout.calendar_activity);
+        init();
     }
+
+    private void init(){
+        addTreatment = findViewById(R.id.FB_add_treatment);
+        setActions();
+    }
+
+    private void setActions() {
+        addTreatment.setOnClickListener(v -> showTreatmentScreen());
+    }
+
+    private void showTreatmentScreen() {
+        Intent intent = new Intent(this, treatment_screen_controller.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/be/helha/hakem_android_project/controllers/treatment_screen_controller.java
+++ app/src/main/java/be/helha/hakem_android_project/controllers/treatment_screen_controller.java
@@ -2,12 +2,17 @@ package be.helha.hakem_android_project.controllers;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -17,23 +22,41 @@ import be.helha.hakem_android_project.R;
 public class treatment_screen_controller extends AppCompatActivity {
     Button openDatePickerButtonBeginning;
     Button openDatePickerButtonEnd;
-
+    FloatingActionButton addPill;
     TextView beginningDate;
     TextView endDate;
-
     Date beginning;
     Date end;
-    @SuppressLint("MissingInflatedId")
-    @Override
+    LinearLayout mContainer;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_treatment_screen);
+        setContentView(R.layout.add_treatment_activity);
+        init();
+        putFragments();
+    }
+
+    private void init(){
         openDatePickerButtonBeginning = findViewById(R.id.B_DP_beginning);
-        openDatePickerButtonBeginning.setOnClickListener(v -> showDatePickerDialog("beginning"));
         openDatePickerButtonEnd = findViewById(R.id.B_DP_end);
-        openDatePickerButtonEnd.setOnClickListener(v -> showDatePickerDialog("end"));
         beginningDate = findViewById(R.id.TV_beginning_date);
         endDate = findViewById(R.id.TV_end_date);
+        addPill = findViewById(R.id.B_add_pill);
+        mContainer = findViewById(R.id.LL_container);
+        setActions();
+    }
+
+    private void setActions(){
+        openDatePickerButtonBeginning.setOnClickListener(v -> showDatePickerDialog("beginning"));
+        openDatePickerButtonEnd.setOnClickListener(v -> showDatePickerDialog("end"));
+        addPill.setOnClickListener(v -> showPillScreen());
+    }
+
+
+
+    private void showPillScreen() {
+        Intent intent = new Intent(this, pill_screen_controller.class);
+        startActivity(intent);
     }
 
     private void showDatePickerDialog(String Sender) {
@@ -47,7 +70,6 @@ public class treatment_screen_controller extends AppCompatActivity {
             @Override
             public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
                 //Il faut gérer la date ici
-
                 if (Sender.equals("end")) {
                     end = new Date(selectedYear, selectedMonth, selectedDay);
                     endDate.setText(selectedDay + "/" + selectedMonth + "/" + selectedYear);
@@ -55,12 +77,20 @@ public class treatment_screen_controller extends AppCompatActivity {
                     beginning = new Date(selectedYear, selectedMonth, selectedDay);
                     beginningDate.setText(selectedDay + "/" + selectedMonth + "/" + selectedYear);
                 }
-
-
             }
         }, year, month, day);
 
         datePickerDialog.show();
+    }
+
+    private void putFragments(){
+        View partOfDay = getRbFragment();
+        mContainer.addView(partOfDay);
+    }
+
+    private View getRbFragment(){
+        View partOfDay = getLayoutInflater().inflate(R.layout.part_rb_fragment, null);
+        return partOfDay;
     }
 
 
