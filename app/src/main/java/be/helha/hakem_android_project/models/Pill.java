386ad3824b1817bc66bccdbf33551c980b1e1159
TheoHakk <102266628+TheoHakk@users.noreply.github.com>
2023-11-22 09:54:16 +0100

    Java doc finie pour le modèle

--- app/src/main/java/be/helha/hakem_android_project/models/Pill.java
+++ app/src/main/java/be/helha/hakem_android_project/models/Pill.java
@@ -3,6 +3,10 @@ package be.helha.hakem_android_project.models;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * The Pill class represents a medication pill with a name, duration, parts of the day,
+ * and an optional identifier for the database.
+ */
 public class Pill implements Serializable {
 
     private String mName;
@@ -10,42 +14,91 @@ public class Pill implements Serializable {
     private List<PartOfDay> mPartsOfDay;
     private int mId;
 
+    /**
+     * Constructs a new instance of the Pill class.
+     *
+     * @param mName      The name of the pill.
+     * @param mDuration  The recommended duration of the pill.
+     * @param partOfDays The list of parts of the day for which the pill is prescribed.
+     */
     public Pill(String mName, int mDuration, List<PartOfDay> partOfDays) {
         this.mName = mName;
         this.mDuration = mDuration;
         this.mPartsOfDay = partOfDays;
     }
-    public Pill(String mName, int mDuration, List<PartOfDay> partOfDays, int id) {
-        this.mName = mName;
-        this.mDuration = mDuration;
-        this.mPartsOfDay = partOfDays;
+
+    /**
+     * Constructs a new instance of the Pill class with an identifier.
+     *
+     * @param name       The name of the pill.
+     * @param duration   The duration of the pill.
+     * @param partsOfDay The list of parts of the day for which the pill is prescribed.
+     * @param id         The identifier of the pill.
+     */
+    public Pill(String name, int duration, List<PartOfDay> partsOfDay, int id) {
+        this(name, duration, partsOfDay);
         this.mId = id;
     }
 
+    /**
+     * Gets the name of the pill.
+     *
+     * @return The name of the pill.
+     */
     public String getName() {
         return mName;
     }
 
+    /**
+     * Gets the duration of the pill.
+     *
+     * @return The duration of the pill.
+     */
     public int getDuration() {
         return mDuration;
     }
 
+    /**
+     * Gets the list of parts of the day for which the pill is prescribed.
+     *
+     * @return The list of parts of the day.
+     */
     public List<PartOfDay> getPartsOfDay() {
         return mPartsOfDay;
     }
 
+    /**
+     * Sets the name of the pill.
+     *
+     * @param mName The new name of the pill.
+     */
     public void setName(String mName) {
         this.mName = mName;
     }
 
+    /**
+     * Sets the duration of the pill.
+     *
+     * @param mDuration The new duration of the pill.
+     */
     public void setDuration(int mDuration) {
         this.mDuration = mDuration;
     }
 
+    /**
+     * Sets the list of parts of the day for which the pill is prescribed.
+     *
+     * @param mPartsOfDay The new list of parts of the day.
+     */
     public void setPartsOfDay(List<PartOfDay> mPartsOfDay) {
         this.mPartsOfDay = mPartsOfDay;
     }
 
+    /**
+     * Gets the identifier of the pill.
+     *
+     * @return The identifier of the pill.
+     */
     public int getId() {
         return mId;
     }
--- app/src/main/java/be/helha/hakem_android_project/models/Treatment.java
+++ app/src/main/java/be/helha/hakem_android_project/models/Treatment.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.List;
 
+/**
+ * The Treatment class represents a medication treatment with a pill, parts of the day,
+ * and the beginning and end dates of the treatment. Also we have the id for the database.
+ */
 public class Treatment implements Serializable {
 
     private int mId;
@@ -12,62 +16,128 @@ public class Treatment implements Serializable {
     private Calendar mBeginning;
     private Calendar mEnd;
 
-    public Treatment(Pill pill, List<PartOfDay> partsOfDay, Calendar mBeginning, Calendar end) {
+    /**
+     * Constructs a new instance of the Treatment class.
+     *
+     * @param pill       The Pill object representing the medication.
+     * @param partsOfDay The list of parts of the day for which the medication is prescribed.
+     * @param beginning  The beginning date of the treatment.
+     * @param end        The end date of the treatment.
+     */
+    public Treatment(Pill pill, List<PartOfDay> partsOfDay, Calendar beginning, Calendar end) {
         this.mPill = pill;
         this.mPartsOfDay = partsOfDay;
-        this.mBeginning = mBeginning;
+        this.mBeginning = beginning;
         this.mEnd = end;
     }
 
-    public void setId(int mId) {
-        this.mId = mId;
+    /**
+     * Sets the identifier of the treatment.
+     *
+     * @param id The new identifier of the treatment.
+     */
+    public void setId(int id) {
+        this.mId = id;
     }
 
+    /**
+     * Gets the identifier of the treatment.
+     *
+     * @return The identifier of the treatment.
+     */
     public int getId() {
         return this.mId;
     }
 
-    public void setPill(Pill mPill) {
-        this.mPill = mPill;
+    /**
+     * Sets the pill of the treatment.
+     *
+     * @param pill The new Pill object representing the medication.
+     */
+    public void setPill(Pill pill) {
+        this.mPill = pill;
     }
 
-    public void setPartsOfDay(List<PartOfDay> mPartsOfDay) {
-        this.mPartsOfDay = mPartsOfDay;
+    /**
+     * Sets the list of parts of the day for which the medication is prescribed.
+     *
+     * @param partsOfDay The new list of parts of the day.
+     */
+    public void setPartsOfDay(List<PartOfDay> partsOfDay) {
+        this.mPartsOfDay = partsOfDay;
     }
 
-    public void setBeginning(Calendar mBeginning) {
-        this.mBeginning = mBeginning;
+    /**
+     * Sets the beginning date of the treatment.
+     *
+     * @param beginning The new beginning date of the treatment.
+     */
+    public void setBeginning(Calendar beginning) {
+        this.mBeginning = beginning;
     }
 
-    public void setEnd(Calendar mEnd) {
-        this.mEnd = mEnd;
+    /**
+     * Sets the end date of the treatment.
+     *
+     * @param end The new end date of the treatment.
+     */
+    public void setEnd(Calendar end) {
+        this.mEnd = end;
     }
 
+    /**
+     * Gets the Pill object representing the medication.
+     *
+     * @return The Pill object.
+     */
     public Pill getPill() {
         return mPill;
     }
 
+    /**
+     * Gets the list of parts of the day for which the medication is prescribed.
+     *
+     * @return The list of parts of the day.
+     */
     public List<PartOfDay> getPartsOfDay() {
         return mPartsOfDay;
     }
 
+    /**
+     * Gets the beginning date of the treatment.
+     *
+     * @return The beginning date.
+     */
     public Calendar getBeginning() {
         return mBeginning;
     }
 
+    /**
+     * Gets the end date of the treatment.
+     *
+     * @return The end date.
+     */
     public Calendar getEnd() {
         return mEnd;
     }
 
-
+    /**
+     * Gets the formatted string of the beginning date in the format "dd/MM/yyyy".
+     *
+     * @return The formatted beginning date string.
+     */
     public String getBeginningString() {
         int day = mBeginning.get(Calendar.DAY_OF_MONTH);
-        int month = mBeginning.get(Calendar.MONTH) + 1; //We add 1 for getting the real month
+        int month = mBeginning.get(Calendar.MONTH) + 1; // We add 1 for getting the real month
         int year = mBeginning.get(Calendar.YEAR);
-        //We need to format the date to a string with the format dd/mm/yyyy
         return day + "/" + month + "/" + year;
     }
 
+    /**
+     * Gets the formatted string of the end date in the format "dd/MM/yyyy".
+     *
+     * @return The formatted end date string.
+     */
     public String getEndString() {
         int day = mEnd.get(Calendar.DAY_OF_MONTH);
         int month = mEnd.get(Calendar.MONTH) + 1;
@@ -75,9 +145,13 @@ public class Treatment implements Serializable {
         return day + "/" + month + "/" + year;
     }
 
-
+    /**
+     * Checks if a given date is within the range of the treatment.
+     *
+     * @param date The date to be checked.
+     * @return true if the date is within the range, false otherwise.
+     */
     public boolean containsTheDate(Calendar date) {
         return date.getTime().compareTo(mBeginning.getTime()) > 0 && date.getTime().compareTo(mEnd.getTime()) <= 0;
     }
-
 }
